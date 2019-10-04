7f8d7685b833f33d76998b27961c7b9cb0217644
jecaro <jecaro.ferguson@smoothstack.com>
2019-10-04 11:24:39 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/com/ss/lms/dataaccess/DataAccess.java
+++ src/com/ss/lms/dataaccess/DataAccess.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public abstract class DataAccess<T>
 {
-	Connection con;
-	
+    Connection con;
+
     public DataAccess(String connectionInfo) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");  
         con = DriverManager.getConnection(connectionInfo);
     }
-    
+
     public void close() throws SQLException 
     {
         con.close();
