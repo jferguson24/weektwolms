b1100ff62b885931f3906ace41b29c27f6ab6348
Nicolae <nicolae.vasile@smoothstack.com>
2019-10-05 17:23:20 -0400

    created test folder for UserAdmin

---
+++ test/com/ss/lms/service/test/TestUserAdminUpdate.java
@@ -0,0 +1,28 @@
+package com.ss.lms.service.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.ss.lms.service.*;
+
+class TestUserAdminUpdate 
+{
+	private static ServiceAdmin admin = new UserAdmin(null, null, null, null, null, null, null);;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception{
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
